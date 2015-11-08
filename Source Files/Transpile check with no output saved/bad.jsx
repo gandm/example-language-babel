d46453dd22b89f8a2bc12285407a788a7f08ad3b
gandm <graham.f.clark54@gmail.com>
2015-11-08 11:17:48 +0000

    Added examples.

---
+++ Source Files/Transpile check with no output saved/bad.jsx
@@ -0,0 +1,10 @@
+var ExampleApplication = React.createClass({
+  render: function() {
+    var elapsed = Math.round(this.props.elapsed  / 100);
+    var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+    var message =
+      'React has been successfully running for ' + seconds + ' seconds.';
+
+    return <p>{message}</p;
+  }
+});
